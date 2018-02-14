182c0f164baba1c076332d11935c1cf3be96baa3
Shay Hoffman <shayhoffman@gmail.com>
2018-02-13 20:12:48 -0600

    third function

--- 04week/loops-practice.js
+++ 04week/loops-practice.js
@@ -57,6 +57,14 @@ const availableCurrencies = () => {
 }
 availableCurrencies()
 
-// create a function that console logs a string with the currency and the currency rate for each currency in a given object. For example, 'AUD is at a 1.2742 conversion rate'. Use latestExchangeRate as an example to test your function
+// create a function that console logs a string with the currency and the currency rate for each currency in a given object. For example, 'AUD is at a 1.2742 conversion rate'. Use latestExchangeRate as an example to test your function.
+const stringAndCurrency = () => {
+  let rates = Object.keys(latestExchangeRate.rates);
+  let price = Object.values(latestExchangeRate.rates);
+  const rateLoop = rates.forEach((item, key) => {
+    console.log(rates[key] + " is at a " + price[key] + " conversion rate.");
+  })
+};
+stringAndCurrency();
 
 //create a function that takes in an object and console logs the result of the object's multiplyByTen method on each of the exchange rates. Use latestExchangeRate as an example to test your function
