30eb58c1739b0d254bb0db2ffe7ddeb756825d39
Shay Hoffman <shayhoffman@gmail.com>
2018-02-14 19:06:49 -0600

    first loop

--- 04week/loop.js
+++ 04week/loop.js
@@ -1,1 +1,9 @@
 'use strict'
+
+//Use a for loop to console.log each item in the array carsInReverse
+const carsInReverse = ['honda', 'toyota', 'bmw',' jeep', 'ford', 'tesla']
+for(let i = 0; i < carsInReverse.length; i++) {
+  console.log(carsInReverse[i]);
+}
+
+//Create an object (an array with keys and values) called persons with the following data:
