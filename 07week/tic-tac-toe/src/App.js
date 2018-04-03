39df9dcced39aba385e9ae00b945fcefd7e7a683
Shay Hoffman <shayhoffman@gmail.com>
2018-04-03 14:40:25 -0500

    tic tac stuff

--- 07week/tic-tac-toe/src/App.js
+++ 07week/tic-tac-toe/src/App.js
@@ -1,50 +1,106 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import Announcement from './Announcement';
+import Tile from './Tile';
+import ResetButton from './ResetButton'
 
 class App extends Component {
-  constructor(props){
-    super(props);
+  constructor() {
+    super();
     this.state = {
-      playerTurn: 'X',
       board: [
-        [[null],[null],[null]],
-        [[null],[null],[null]],
-        [[null],[null],[null]]
-    ],
-    win: false
+        ' ', ' ', ' ',
+        ' ', ' ', ' ',
+        ' ', ' ', ' ',
+      ],
+      playerTurn: 'X',
+      winner: null //not sure if this should be null or ''
     }
   }
 
-  render() {
-    const rowStyle = {
-  backgroundColor: 'pink',
-  color: 'red',
-  height: '120px',
-  fontSize: '40px',
-  margin: '5px auto',
-  display: 'flex',
-  flexDirection: 'row',
-  justifyContent: 'center'
+  updateBoard(loc, player) {
+    if(this.state.board[loc] === 'X' || this.state.board === 'O' || this.state.winner) {
+      return;
+    }
+    const currentBoard = this.state.board;
+    currentBoard.splice (loc, 1, this.state.playerTurn);
+    this.setState({board: currentBoard});
+
+
+    const topRow = this.state.board[0] + this.state.board[1] + this.state.board[2];
+    if (topRow.match (/XXX|OOO/)){
+      this.setState({winner: this.state.playerTurn});
+      return;
+    }
+    const middleRow = this.state.board[3] + this.state.board[4] + this.state.board[5];
+    if (middleRow.match(/XXX|OOO/)){
+      this.setState({winner: this.state.playerTurn});
+    }
+    const bottomRow = this.state.board[6] + this.state.board[7] + this.state.board[8];
+    if (bottomRow.match(/XXX|OOO/)){
+      this.setState({winner: this.state.playerTurn});
+    }
+    const leftColumn = this.state.board[0] + this.state.board [3] + this.state.board[6];
+    if (leftColumn.match(/XXX|OOO/)){
+      this.setState({winner: this.state.playerTurn});
+    }
+    const middleColumn = this.state.board[1] + this.state.board[4] + this.state.board[7];
+    if (middleColumn.match(/XXX|OOO/)){
+      this.setState({winner: this.state.playerTurn});
+    }
+    const rightColumn = this.state.board[2] + this.state.board[5] + this.state.board[8];
+    if(rightColumn.match(/XXX|OOO/)){
+      this.setState({winner: this.state.playerTurn});
+    }
+    const rightDiagonal = this.state.board[2] + this.state.board[4] + this.state.board[6];
+    if (rightDiagonal.match(/XXX|OOO/)){
+      this.setState({winner: this.state.playerTurn});
+    }
+    const leftDiagonal = this.state.board[0] + this.state.board[4] + this.state.board[8];
+    if (leftDiagonal.match(/XXX|OOO/)){
+      this.setState({winner: this.state.playerTurn});
+      return;
+    }
+    const moves = this.state.board.join('').replace(/ /g, '');
+    if (moves.length === 9) {
+      alert('this game is a draw: reset the board');
+    }
+    this.setState({playerTurn: (this.state.playerTurn === 'X') ? 'O' : 'X'});
   }
 
-  const boxStyle = {
-  height: '110px',
-  width: '110px',
-  backgroundColor: 'pink',
-  margin: 'auto 20px',
-  border: '1px solid black'
+  resetBoard() {
+    this.setState({
+      board: [
+        ' ', ' ', ' ',
+        ' ', ' ', ' ',
+        ' ', ' ', ' ',
+      ],
+      playerTurn: 'X',
+      winner: null
+    });
   }
+//change google font later
+  render() {
     return (
-      <div className="App">
-        {this.state.board.map((row, index) => {
-          return <div key={index} style={rowStyle}>
-        {row.map((box, index) => {
-          return <div key = {index} style={boxStyle}>{row[index]}</div>
-        })}
-        </div>
-      })}
-      </div>
+      <div className = "container">
+        <div className = "menu">
+          <link href= "https://fonts.googleapis.com/css?famioly=Press+Star2P" rel = "stylesheet"/>
+          <h1>Welcome to TicTacToe!</h1>
+          <Announcement winner = {this.state.winner}/>
+          </div>
+          {this.state.board.map(function(value, index) {
+            return(
+              <Tile
+                key = {index}
+                loc = {index}
+                value = {value}
+                updateBoard = {this.updateBoard.bind(this)}
+                playerTurn = {this.state.playerTurn}/>
+            );
+          }.bind(this))}
+          <ResetButton reset = {this.resetBoard.bind(this)}/>
+          </div>
     );
   }
 }
